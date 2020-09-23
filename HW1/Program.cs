476427bc110df729e6d2d8eda6038c5a24c8475b
Yair Goldshtein <irigo1@gmail.com>
2020-09-23 16:15:43 +0300

    Add project files.

---
+++ HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random(DateTime.Now.Millisecond);
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine(localDate);
+        }
+    }
+}
