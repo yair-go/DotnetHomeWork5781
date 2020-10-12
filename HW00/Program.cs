664cd4d4c23f8d2ad01b302b81e2af86fbeb20c4
Yair Goldshtein <irigo1@gmail.com>
2020-10-12 11:40:31 +0300

    Hello Eliezer

--- HW00/Program.cs
+++ HW00/Program.cs
@@ -10,7 +10,7 @@ namespace HW00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello Eliezer");
             Console.ReadKey();
         }
     }
