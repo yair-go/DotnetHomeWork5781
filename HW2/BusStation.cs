160cf5bfe0dd11d34beb6e98e749c2c1b974e703
Yair Goldshtein <irigo1@gmail.com>
2020-10-22 13:13:10 +0300

    HW2, first commit, BusStation

---
+++ HW2/BusStation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class BusStation
+    {
+        #region *** private fields ***
+
+        private int stationCode;
+        private Double latitude;
+        private Double longitude;
+        #endregion
+
+        #region *** constructors ***
+        public BusStation(int stationCode, Double latitude, Double longitude)
+        {
+            this.stationCode = stationCode;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+
+        #endregion
+
+        #region *** properties ***
+        public int StationCode { get => stationCode;  }
+        public Double Latitude { get => latitude;  }
+        public Double Longitude { get => longitude; }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Bus Station Code: {stationCode} {latitude}N {longitude}E";
+        }
+
+    }
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusStation busStation = new BusStation(123456, 31.766101, 35.192826);
+
+            Console.WriteLine(busStation);
+        }
+    }
+}
